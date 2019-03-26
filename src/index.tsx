86249f9cb44fc32ad7d53c1fd85e09d2adaea327
bacti <hai.tranminh@gameloft.com>
2019-03-26 12:09:53 +0700

    +

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,8 @@ class SpeechToText extends Component
     [x: string]: any
     audioContext = new AudioContext()
     state = { url_broadcast: '' }
+    noiseMag = 0.01
+    lastMag = 0
 
     constructor()
     {
@@ -38,6 +40,44 @@ class SpeechToText extends Component
             alert('getUserMedia() error: ' + e)
             console.log(e)
         })
+    }
+
+    ProcessAudioBuffer(event)
+    {
+        let buffer = event.inputBuffer.getChannelData(0)
+        let outBuffer = event.outputBuffer.getChannelData(0)
+
+        let avgMag = buffer.reduce((acc, val) => acc + Math.abs(val), 0) / buffer.length
+        if (avgMag < this.noiseMag * 2.5)
+        {
+            this.noiseMag = this.noiseMag * 0.95 + 0.05 * avgMag
+            this.noiseMag = Math.min(0.05, this.noiseMag)
+        }
+        this.lastMag = avgMag
+        console.log((this.lastMag / this.noiseMag).toFixed(3))
+
+        if (this.recording) {
+            let copyBuffer = buffer.slice()
+            this.recordingData.push(copyBuffer)
+            // outBuffer.set(buffer)
+
+            // End-Of-Utterance recognization
+            if (avgMag > this.noiseMag * SPEECH_THRESHOLD) {
+                this.speech++
+                this.silent = 0
+            } else {
+                this.silent++
+            }
+
+            // Trim audio if user not speech yet
+            if (this.speech < 4 && this.recordingData.length > 10) {
+                this.recordingData = this.recordingData.slice(this.recordingData.length - 10)
+                this.speech = Math.max(0, this.speech - 0.2)
+            }
+        }
+        else {
+            outBuffer.fill(0)
+        }
     }
 
     Ref(ref, object)
