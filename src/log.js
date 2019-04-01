5c08946d8afd9e2f72b07d4c0c14596cc7dd1e7d
bacti <hai.tranminh@gameloft.com>
2019-04-01 15:33:29 +0700

    +

--- src/log.js
+++ src/log.js
@@ -3,7 +3,7 @@ export const Trace = message =>
     const log = `${(window.performance.now() / 1000).toFixed(3)}: ${message + []}\n`
     console.warn(log)
     const logdiv = document.getElementById('log')
-    logdiv && (document.getElementById('log').innerText += log)
+    logdiv && (logdiv.innerText = log + logdiv.innerText)
 }
 
 export const Error = message =>
